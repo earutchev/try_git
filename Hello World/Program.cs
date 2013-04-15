f3ea677f0449325736ac2610ed3834b4b15fd4d7
earutchev <earutchev@fanfueled.com>
2013-04-15 14:38:11 -0500

    Hello Wrold

---
+++ Hello World/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Hello_World
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
